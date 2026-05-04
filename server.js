fa8daf2703c58e9f0dd6881d2adf959eb9d77468
Konstantin Oralkin <ora@Konstantins-MacBook-Air.local>
2026-05-04 22:40:08 +0300

    Add CORS headers

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const Browserbase = require('@browserbasehq/sdk');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 app.use(express.json());
 app.use(express.static('public'));
 
